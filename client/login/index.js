ca19577face4187ccdc238814f188160e518f19c
Antoine Krull <a.krull@campus.lmu.de>
2023-12-11 16:26:45 +0100

    added folder structure and files for login ui

---
+++ client/login/index.js
@@ -0,0 +1,7 @@
+function submitForm() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    console.log("Username: " + username);
+    console.log("Password: " + password);
+}
