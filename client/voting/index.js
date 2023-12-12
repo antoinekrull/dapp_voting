11e14a9240002e8bfb52920ea0049cc06ec781e4
Antoine Krull <a.krull@campus.lmu.de>
2023-12-12 13:14:56 +0100

    added voting template

---
+++ client/voting/index.js
@@ -0,0 +1,5 @@
+function auswahlAnzeigen(ausgewaehlterKandidat) {
+    // Die Auswahl in einem Element anzeigen
+    var auswahlElement = document.getElementById("auswahl");
+    auswahlElement.innerHTML = "Ausgewählter Kandidat: " + ausgewaehlterKandidat;
+}
