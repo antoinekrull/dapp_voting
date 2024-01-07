81539164c0001ce46435e56a4eef2124f6fac221
I1 <egeguler2003@gmail.com>
2024-01-07 16:50:09 +0100

    ADD: Mock Oracle to Simulate usage of Oracles in blockchain
    Mock Oracle provides necessary function to generate random key for each voter.
    A key can only vote for once, and user's address is not saved to ensure transparency of election.
    This key is issued by an external, trusted and independent party such as an electoral board. Mock
    oracle simulates this phenomena.

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,7 +1,18 @@
 // 2_deploy_voting.js
+/*
 const Voting = artifacts.require("Voting");
 
 module.exports = function (deployer) {
   // Deploy the Voting contract
   deployer.deploy(Voting);
+};
+
+*/
+const MockOracle = artifacts.require("MockOracle");
+const Voting = artifacts.require("Voting");
+
+module.exports = function (deployer) {
+  deployer.deploy(MockOracle).then(function () {
+    return deployer.deploy(Voting, MockOracle.address);
+  });
 };
