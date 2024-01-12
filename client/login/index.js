aaf4b4ebd9fb74d521f850de7146cac511f960d4
Antoine Krull <a.krull@campus.lmu.de>
2024-01-12 11:19:46 +0100

    login page checks if provided address is blockchain address; minor changes

--- client/login/index.js
+++ client/login/index.js
@@ -1,5 +1,20 @@
+// import Web3 from 'web';
+
 function submitForm() {
     var address = document.getElementById("address").value;
+    var errorMessageElement = document.getElementById("error-message");
+    if(!isValidBlockchainAddress(address)){
+        errorMessageElement.innerHTML = "Eingabe ist keine gültige Blockchain-Adresse.";
+        errorMessageElement.style.display = "block"; // Zeige die Fehlermeldung an
+        return;
+    }
     window.location.href = '../voting/index.html';
     console.log("address: " + address);
+}
+
+function isValidBlockchainAddress(address) {
+    // eth address consists of 40 hexadecimal signs
+    const ethAddressRegex = /^(0x)?[0-9a-fA-F]{40}$/;
+
+    return ethAddressRegex.test(address);
 }
--- client/voting/index.js
+++ client/voting/index.js
@@ -1,4 +1,15 @@
+var electionsStarted = false; // Variable to track whether elections have started
+
+function startElections() {
+    electionsStarted = true;
+    document.getElementById("election-status").style.display = "none"; // Hide the status message
+}
+
 function auswahlAnzeigen(ausgewaehlterKandidat) {
+    console.log(ausgewaehlterKandidat)
+    if (!electionsStarted) {
+        return; // Do nothing if elections haven't started
+    }
     // Die Auswahl in einem Element anzeigen
     var auswahlElement = document.getElementById("auswahl");
     auswahlElement.innerHTML = "Ausgewählter Kandidat: " + ausgewaehlterKandidat;
