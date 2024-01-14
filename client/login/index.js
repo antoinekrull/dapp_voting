edd46bd608a594aa54f01dc678c51b222708a5b7
Antoine Krull <a.krull@campus.lmu.de>
2024-01-14 12:08:10 +0100

    wip: resolving undeclared method error

--- client/login/index.js
+++ client/login/index.js
@@ -1,6 +1,17 @@
-// import Web3 from 'web';
+import Web3 from 'web3';
+import contractInfo from '../../contracts/Voting.sol';
+const web3 = new Web3(Web3.givenProvider);
 
-function submitForm() {
+// address of the contract
+const contractAddress = contractInfo.networks['5777'].address;
+
+// TODO: thats not what the IDE proposed me, try .contractAbi
+const contractABI = contractInfo.abi;
+
+// Get a contract instance
+const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+export function submitForm(){
     var address = document.getElementById("address").value;
     var errorMessageElement = document.getElementById("error-message");
     if(!isValidBlockchainAddress(address)){
@@ -8,6 +19,7 @@ function submitForm() {
         errorMessageElement.style.display = "block";
         return;
     }
+    safeAddressInContract(address);
     window.location.href = '../voting/index.html';
     console.log("address: " + address);
 }
@@ -18,3 +30,23 @@ function isValidBlockchainAddress(address) {
 
     return ethAddressRegex.test(address);
 }
+
+function safeAddressInContract(address){
+    contract.methods.safeUserAddress().send({ from: address })
+        .on('transactionHash', function (hash) {
+            console.log('Transaction Hash:', hash);
+        })
+        .on('confirmation', function (confirmationNumber, receipt) {
+            console.log('Confirmation:', confirmationNumber, receipt);
+        })
+        .on('receipt', function (receipt) {
+            console.log('Receipt:', receipt);
+        })
+        .on('error', function (error) {
+            console.error('Error:', error);
+        });
+}
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.querySelector('button').addEventListener('click', submitForm);
+// });
